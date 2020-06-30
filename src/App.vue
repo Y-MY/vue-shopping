f39cf1f8b194054d19d492db4a1dbd657916ff75
Y-MY <784519047@qq.com>
2020-06-30 23:24:58 +0800

    header footer

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,28 @@
-<!--
 <template>
-  <div class="content">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <my-header></my-header>
+    <div class="content">
+      <!--<router-view/>-->
+      111
+    </div>
+    <my-footer></my-footer>
   </div>
 
 </template>
 
 <script>
+  import MyHeader from './components/layout/MyHeader.vue';
+  import MyFooter from './components/layout/MyFooter.vue';
+
   export default {
-    name: 'App'
+    name: 'App',
+    components: {MyHeader,MyFooter},
   }
 </script>
 
 <style lang="less" rel="stylesheet/less">
-  .content {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-</style>
--->
-<template>
-  <div>
-    <p class="link" :class="className">1111</p>
-    <p class="link" :class="classNameArr">1111</p>
-    <p class="link" :class="[classA,classB]">1111</p>
-    <p class="link" :class="[classA,{'red-font':hasError}]">1111</p>
-    <p class="link" :style="linkCss">1111</p>
-    <p class="link" :style="linkCss">1111</p>
-
-    <p v-if="isPartA">partA</p>
-    <p v-show="!isPartA">partB</p>
-
-    <p v-if="isPartA">partA</p>
-    <p v-else>no data</p>
-
-    <button v-on:click="toggle">toggle</button>
-  </div>
-</template>
-
-<script>
-  export default {
-    data() {
-      return {
-        className: {
-          'red-font': true,
-          'blue-font': false
-        },
-        classNameArr: ['red-font', 'big-font'],
-        classA: 'big-font',
-        classB: 'red-font',
-        hasError: false,
-        linkCss: {
-          'color': 'red',
-          'font-size': '30px',
-        },
-        isPartA:true,
-      }
-    },
-    methods: {
-      toggle(){
-        this.isPartA = !this.isPartA;
-      }
-    }
-  }
-
-</script>
-
-<style lang="less">
-  .red-font {
-    color: red;
-  }
-
-  .big-font {
-    font-size: 20px;
+  @import './less/common.less';
+  .content{
+    margin:50px 0;
   }
 </style>
---
+++ src/components/layout/MyFooter.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="app-foot">
+    <p>© 2020 Y-MY MIT</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+    },
+    data: function () {
+      return {
+        isShowAbout: false,
+        isShowLogin: false,
+        isShowReg: false
+      }
+    },
+    methods: {
+      showDialog (param) {
+        this[param] = true
+      },
+      hideDialog (param) {
+        this[param] = false
+      }
+    }
+  }
+</script>
+<style lang="less" scoped>
+  .app-foot {
+    position: absolute;
+    bottom: 0;
+    text-align: center;
+    height: 50px;
+    width: 100%;
+    line-height: 50px;
+    background: #e3e4e8;
+    clear: both;
+    margin-top: 30px;
+  }
+</style>
